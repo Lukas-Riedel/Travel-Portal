0dcb03a2e496bb3d54b68ea5a538ad016eeebe6d
Lukáš Riedel <lukas.riedel@getmanta.com>
2024-09-29 17:21:01 +0200

    Introduce database migration system

---
+++ Web/php/deploy.php
@@ -0,0 +1,69 @@
+<?php
+    require_once(dirname(__FILE__) . "/provider/DatabaseProvider.php");
+
+    $databaseProvider = new DatabaseProvider();
+    
+    $onError = function($level, $message, $file, $line) {
+        throw new RuntimeException($message);
+    };
+
+    $migrationScriptsBasePath = dirname(__FILE__) . "/../sql/";
+    $migrationScriptFileNames = array_map(function ($path) { 
+        $tokens = explode("/", $path);
+        return $tokens[count($tokens) - 1];
+     }, array_filter((array) glob($migrationScriptsBasePath . "*")));
+    asort($migrationScriptFileNames);
+
+    $alreadyAppliedScriptsRows = $databaseProvider
+        ->statementBuilder("SELECT * FROM migration_script")
+        ->getResultSet();
+
+    $alreadyAppliedScripts = array();
+    foreach ($alreadyAppliedScriptsRows as &$alreadyAppliedScriptsRow) {
+        $alreadyAppliedScripts[$alreadyAppliedScriptsRow["name"]] = array(
+            "hash" => $alreadyAppliedScriptsRow["hash"],
+            "timestamp" => $alreadyAppliedScriptsRow["timestamp"]);
+    }
+
+    foreach ($migrationScriptFileNames as &$migrationScriptFileName) {
+        $path = $migrationScriptsBasePath . $migrationScriptFileName;
+        $hash = hash_file("sha256", $path);
+
+        if (!array_key_exists($migrationScriptFileName, $alreadyAppliedScripts)) {
+            $handle = fopen($path, "r");
+            $migrationScript = fread($handle, filesize($path));
+            fclose($handle);
+
+            $databaseProvider->beginTransaction();
+            try {
+                set_error_handler($onError);
+
+                $databaseProvider
+                    ->statementBuilder($migrationScript)
+                    ->execute();
+
+                $databaseProvider
+                    ->statementBuilder("INSERT INTO migration_script (name, hash, timestamp) VALUES (?, ?, UNIX_TIMESTAMP())")
+                    ->withParameters($migrationScriptFileName, $hash)
+                    ->execute();
+                
+                $databaseProvider->commit();
+            }
+            catch (Throwable $e) {
+                $databaseProvider->rollback();
+                http_response_code(500);
+                throw $e;
+            }
+            finally {            
+                restore_error_handler();
+            }
+        }
+        else if ($hash != $alreadyAppliedScripts[$migrationScriptFileName]["hash"]) {
+            http_response_code(500);
+            die("Could not apply " . $migrationScriptFileName . " migration script. It was already applied at " . date('d.m.Y H:i:s', $alreadyAppliedScripts[$migrationScriptFileName]["timestamp"]) . ".");
+        }
+    }
+    
+    http_response_code(200);
+    echo "The database was successfully migrated.";
+?>
