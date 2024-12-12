317ff8d605c7e5f14cda63361f94022593540ba0
Lukáš Riedel <thespeedding@gmail.com>
2024-12-12 22:55:29 +0100

    Adapt to new Photos Library API scopes

--- Web/js/common/constants.js
+++ Web/js/common/constants.js
@@ -4,9 +4,8 @@ let api = undefined;
 const now = new Date(Date.now()).getTime() / 1000;
 
 const googleApiAuthorizationScopes = [
-    "https://www.googleapis.com/auth/photoslibrary.readonly",
     "https://www.googleapis.com/auth/photoslibrary.appendonly",
-    "https://www.googleapis.com/auth/photoslibrary.sharing",
+    "https://www.googleapis.com/auth/photoslibrary.readonly.appcreateddata",
     "https://www.googleapis.com/auth/photoslibrary.edit.appcreateddata",
     "https://www.googleapis.com/auth/fitness.activity.read",
     "https://www.googleapis.com/auth/fitness.location.read",
