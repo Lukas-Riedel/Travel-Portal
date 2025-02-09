4fe001b646186266ea68b846676bcd2be91b172a
Lukáš Riedel <Lukas.Riedel@ibm.com>
2025-02-09 17:13:53 +0100

    Fix issue in PhotoPrototype

--- Agent/src/main/java/cz/lriedel/agent/model/request/PhotoPrototype.java
+++ Agent/src/main/java/cz/lriedel/agent/model/request/PhotoPrototype.java
@@ -2,7 +2,7 @@ package cz.lriedel.agent.model.request;
 
 import javax.annotation.Nullable;
 
-public record PhotoPrototype(String name, @Nullable Integer position, @Nullable Long replacePhotoId, String data) {
+public record PhotoPrototype(String name, @Nullable Integer position, @Nullable Long replacedPhotoId, String data) {
 
     public PhotoPrototype(String name, int position, String data) {
         this(name, position, null, data);
