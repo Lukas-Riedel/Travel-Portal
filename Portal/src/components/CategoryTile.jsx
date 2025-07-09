3ea28028d4dbd5a9935ffe08c860ad06b25fbdb8
Lukáš Riedel <thespeedding@gmail.com>
2025-07-09 22:25:55 +0200

    Fix issue with CategoryTile rendering

--- Portal/src/components/CategoryTile.jsx
+++ Portal/src/components/CategoryTile.jsx
@@ -1,7 +1,7 @@
 import PhotoTile from "./PhotoTile"
 
 export default function CategoryTile({ category }) {
-    return (
+    return (!category || category.mainHighlight) && (
         <PhotoTile
             src={category?.mainHighlight?.url?.thumbnail ?? category?.mainHighlight?.url?.full}
             firstLineText={category?.name}
--- Portal/src/components/TripTile.jsx
+++ Portal/src/components/TripTile.jsx
@@ -1,5 +1,4 @@
 import { useMemo } from "react"
-import { useRegularPlaces } from "../hooks/useRegularPlaces"
 import { getDateRangeString } from "../utils/helpers"
 import PhotoTile from "./PhotoTile"
 import { useCategories } from "../hooks/useCategories"
