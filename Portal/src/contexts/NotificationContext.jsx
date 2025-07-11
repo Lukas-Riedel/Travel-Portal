e9896522dff58a7b7affc88f4bce6d88f9f53822
Lukáš Riedel <Lukas.Riedel@ibm.com>
2025-07-11 14:56:32 +0200

    Register FCM token after every access token change

--- Portal/src/contexts/NotificationContext.jsx
+++ Portal/src/contexts/NotificationContext.jsx
@@ -2,13 +2,16 @@ import { createContext, useContext, useEffect, useState } from "react"
 import { getToken, onMessage } from "firebase/messaging"
 import { messaging } from "../lib/firebase"
 import { useApi } from "../hooks/useApi"
+import { useAuth } from "./AuthContext"
 
 const NotificationContext = createContext()
 
 export const NotificationProvider = ({ children }) => {
+    const { isAdmin } = useAuth()
     const { createDevice } = useApi()
 
     const [messages, setMessages] = useState([])
+    const [fcmToken, setFcmToken] = useState(null)
 
     useEffect(() => {
         if ("serviceWorker" in navigator) {
@@ -17,13 +20,13 @@ export const NotificationProvider = ({ children }) => {
                     vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
                     serviceWorkerRegistration: registration
                 }))
-                .then(createDevice)
+                .then(setFcmToken)
                 .catch(e => console.log(e))
         }
 
         const unsubscribe = onMessage(messaging, payload => {
             const deserializedArgs = payload.data?.args ? JSON.parse(payload.data.args) : undefined
-            
+
             setMessages(prev => [
                 ...prev,
                 {
@@ -38,6 +41,12 @@ export const NotificationProvider = ({ children }) => {
 
         return () => unsubscribe()
     }, [])
+
+    useEffect(() => {
+        if (fcmToken) {
+            createDevice(fcmToken)
+        }
+    }, [fcmToken, isAdmin])
 
     return (
         <NotificationContext.Provider value={{ messages }}>
