73d23542feaef6274c2bc9fda11225b40f6ec1c3
Lukáš Riedel <Lukas.Riedel@ibm.com>
2025-07-18 23:05:18 +0200

    Compute remaining upload time more realistically

--- Portal/src/components/DayCard.jsx
+++ Portal/src/components/DayCard.jsx
@@ -86,45 +86,19 @@ export default function DayCard({ day, events, stay, fitness, publicHoliday, tim
     }
 
     function RemainingUploadTime({ album }) {
-        const { uploadingProgress } = album
         const [remaining, setRemaining] = useState(null)
 
-        const lastProgress = useRef(null)
-        const lastTimestamp = useRef(null)
-        const deadline = useRef(null)
+        useEffect(() => {
+            if (album.uploadingProgress > 0 && album.uploadingProgress < 0) {
+                const now = Date.now() / 1000
+                const elapsed = now - album.uploadingStart
+                setRemaining(elapsed * 100 / album.uploadingProgress - elapsed)
+            }
+        }, [album])
 
         useEffect(() => {
-            const now = Date.now() / 1000
-
-            if (lastProgress.current === null) {
-                lastProgress.current = uploadingProgress
-                lastTimestamp.current = now
-                return
-            }
-
-            const progressDiff = uploadingProgress - lastProgress.current
-            const timeDiff = now - lastTimestamp.current
-
-            if (progressDiff > 0 && timeDiff > 0) {
-                const speed = progressDiff / timeDiff
-                const secsToGo = (100 - uploadingProgress) / speed
-                deadline.current = now + secsToGo
-                setRemaining(secsToGo)
-            }
-
-            lastProgress.current = uploadingProgress
-            lastTimestamp.current = now
-        }, [uploadingProgress])
-
-        useEffect(() => {
-            const iv = setInterval(() => {
-                if (deadline.current != null) {
-                    const now = Date.now() / 1000
-                    const rem = Math.max(0, deadline.current - now)
-                    setRemaining(rem)
-                }
-            }, 1000)
-            return () => clearInterval(iv)
+            const interval = setInterval(() => setRemaining(prev => !prev ? prev : Maht.max(0, prev - 1)), 1000)
+            return () => clearInterval(interval)
         }, [])
 
         return remaining && (
@@ -133,7 +107,6 @@ export default function DayCard({ day, events, stay, fitness, publicHoliday, tim
             </span>
         )
     }
-
 
     return day && events ? ((events.length > 0 || stay) && (
         <div className={`rounded-xl p-4 h-full flex flex-col ${isToday ? "bg-gray-100 border border-gray-400 text-gray-900 shadow-lg" : "shadow-md bg-white"}`}>
