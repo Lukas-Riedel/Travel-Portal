006c976eb018049f8cc6b5b74bc0ab492dc5b546
Lukáš Riedel <thespeedding@gmail.com>
2025-08-06 21:05:13 +0200

    Introduce parallel workers

--- Service/public/worker.php
+++ Service/public/worker.php
@@ -5,14 +5,30 @@
         throw new RuntimeException($message);
     };
     set_error_handler($onError);
-    
-    $key = ftok(__FILE__, 1);
-    $semaphore = sem_get($key);
 
-    if (sem_acquire($semaphore, TRUE)) {
-        $eventManager->handleEvents();
-        sem_release($semaphore);
+    $lock = NULL;
+    $lockFile = NULL;
+
+    for ($i = 0; $i < WORKERS_COUNT; ++$i) {
+        $path = __DIR__ . "/worker.lock." . $i;
+        $handle = fopen($path, "w+");
+        if ($handle && flock($handle, LOCK_EX | LOCK_NB)) {
+            $lock = $handle;
+            $lockFile = $path;
+            break;
+        } 
+        else {
+            fclose($handle);
+        }
     }
-    
-    restore_error_handler();
+
+    if ($lock) {
+        try {
+            $eventManager->handleEvents();
+        }
+        finally {
+            flock($lock, LOCK_UN);
+            fclose($lock);
+        }
+    }
 ?>
