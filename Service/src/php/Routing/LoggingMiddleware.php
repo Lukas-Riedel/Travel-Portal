c110415f86f54bb08f74b1ad5eb644f9c5d4dac3
Lukáš Riedel <Lukas.Riedel@ibm.com>
2025-08-12 07:22:05 +0200

    Improve request logging

--- Service/src/php/Routing/LoggingMiddleware.php
+++ Service/src/php/Routing/LoggingMiddleware.php
@@ -17,9 +17,9 @@
 
         public function process(ServerRequestInterface $request, RequestHandlerInterface $handler) : ResponseInterface {
             $start = microtime(TRUE);
-            $this->logger->debug("Received the '" . $this->formatRequest($request) . "' request...");
+            $this->logger->debug("Received the '" . $this->formatRequest($request) . "' request...", array("headers" => $request->getHeaders(), "payload" => $request->getParsedBody()));
             $response = $handler->handle($request);
-            $this->logger->info("The '" . $this->formatRequest($request) . "' request was processed in " . round((microtime(TRUE) - $start) * 1000) . " milliseconds.");
+            $this->logger->info("The '" . $this->formatRequest($request) . "' request was processed in " . round((microtime(TRUE) - $start) * 1000) . " milliseconds.", array("headers" => $request->getHeaders(), "payload" => $request->getParsedBody()));
             return $response;
         }
 
