370966e6c75f75ed3018cc228d25f9442cb1138a
Lukáš Riedel <lukas.riedel24@gmail.com>
2025-08-17 12:11:13 +0300

    Update index.php

--- Core/public/index.php
+++ Core/public/index.php
@@ -6,8 +6,8 @@
     use Core\Routing\ErrorHandlingMiddleware;
     use Core\Routing\JsonInvocationStrategy;
     use Core\Routing\LoggingMiddleware;
-use Core\Routing\PayloadDecodingMiddleware;
-use Core\Routing\RequestError;
+    use Core\Routing\PayloadDecodingMiddleware;
+    use Core\Routing\RequestError;
     use Core\Routing\TransactionMiddleware;
     use Slim\Handlers\Strategies\RequestResponse;
 
@@ -19,13 +19,13 @@ use Core\Routing\RequestError;
     $app->getRouteCollector()->setDefaultInvocationStrategy(new JsonInvocationStrategy());
     $app->setBasePath($basePath);
 
-    $app->add(new LoggingMiddleware($logger));
-    $app->add(new ErrorHandlingMiddleware($logger));
-    $app->addBodyParsingMiddleware();
-    $app->add(new PayloadDecodingMiddleware());
-    $app->addRoutingMiddleware();
-    $app->add(new AuthMiddleware($authenticationService, $basePath));
     $app->add(new TransactionMiddleware($databaseProvider));
+    $app->add(new AuthMiddleware($authenticationService, $basePath));
+    $app->addRoutingMiddleware();
+    $app->add(new PayloadDecodingMiddleware());
+    $app->addBodyParsingMiddleware();
+    $app->add(new ErrorHandlingMiddleware($logger));
+    $app->add(new LoggingMiddleware($logger));
 
     (require_once(__DIR__ . "/src/php/routes.php"))($app);
 
