467c7875c8c874582e3eb8c4452d04cccdaec54a
Lukáš Riedel <thespeedding@gmail.com>
2026-04-22 21:56:04 +0200

    Introduce embedding lock

--- Cortex/src/core/ai_engine.py
+++ Cortex/src/core/ai_engine.py
@@ -1,3 +1,4 @@
+import threading
 import base64
 from io import BytesIO
 
@@ -10,17 +11,20 @@ from torch import Tensor
 class AiEngine:
     def __init__(self, model_name: str, engine_device: str) -> None:
         self.model = SentenceTransformer(model_name, device=engine_device)
+        self.lock = threading.Lock()
 
     def get_photo_embedding(self, base64_data: str) -> Tensor:
-        image = self._get_thumbnail_image(base64_data)
-        with torch.no_grad():
-            return self._post_process_embedding(
-                self.model.encode(image, convert_to_tensor=True, normalize_embeddings=True))
+        with self.lock:
+            image = self._get_thumbnail_image(base64_data)
+            with torch.no_grad():
+                return self._post_process_embedding(
+                    self.model.encode(image, convert_to_tensor=True, normalize_embeddings=True))
 
     def get_text_embedding(self, text: str) -> Tensor:
-        with torch.no_grad():
-            return self._post_process_embedding(
-                self.model.encode([text], convert_to_tensor=True, normalize_embeddings=True))
+        with self.lock:
+            with torch.no_grad():
+                return self._post_process_embedding(
+                    self.model.encode([text], convert_to_tensor=True, normalize_embeddings=True))
 
     @staticmethod
     def _post_process_embedding(embedding: Tensor) -> Tensor:
