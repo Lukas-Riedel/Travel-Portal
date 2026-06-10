c6b6e6d6b4a84740ee3f0a17b8623399dc068785
Lukáš Riedel <thespeedding@gmail.com>
2026-06-10 22:05:03 +0200

    Set up API requests retry mechanism in UI

--- Portal/src/clients/coreClient.ts
+++ Portal/src/clients/coreClient.ts
@@ -13,6 +13,7 @@ import { Place } from "../classes/Place.ts"
 import { Trip } from "../classes/Trip.ts"
 import { useAuthStore } from "../hooks/useAuthStore.ts"
 import { GUEST_CREDENTIALS } from "../utils/authenticationUtils.ts"
+import axiosRetry from "axios-retry"
 
 export const refreshPlaceHighlights = async (placeId: string, count: number): Promise<Highlight[]> =>
     coreClient.post<Highlight[]>(createQueryPath(`places/${placeId}/highlights/refresh`,
@@ -906,3 +907,19 @@ const createQueryPath = (path: string, params: Record<string, string | number | 
 }
 
 const extractData = <T>(response: AxiosResponse<T>): T => response.data
+
+const axiosGridRetryCondition = (error: any) => {
+    if (!error.response) {
+        return true
+    }
+    if (error.response.status >= 500 && error.response.status <= 599) {
+        return true
+    }
+    return false
+}
+
+axiosRetry(coreClient, {
+    retries: 3,
+    retryDelay: axiosRetry.exponentialDelay,
+    retryCondition: axiosGridRetryCondition
+})
--- Portal/src/clients/iamClient.ts
+++ Portal/src/clients/iamClient.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 import type { AxiosInstance, AxiosResponse } from "axios"
 import type { IamResponse } from "../types/IamResponse.ts"
+import axiosRetry from "axios-retry"
 
 export const getIamResponseWithCredentials = async (username: string, password: string): Promise<IamResponse> =>
     iamClient.post<IamResponse>("token",
@@ -26,3 +27,19 @@ const iamClient: AxiosInstance = axios.create({
 })
 
 const extractIamResponse = (response: AxiosResponse<IamResponse>): IamResponse => response.data
+
+const axiosGridRetryCondition = (error: any) => {
+    if (!error.response) {
+        return true
+    }
+    if (error.response.status >= 500 && error.response.status <= 599) {
+        return true
+    }
+    return false
+}
+
+axiosRetry(iamClient, {
+    retries: 3,
+    retryDelay: axiosRetry.exponentialDelay,
+    retryCondition: axiosGridRetryCondition
+})
