ed135df0013ffe5ed13d55dedec0ba2c81f16cb6
Lukáš Riedel <lukas.riedel@getmanta.com>
2026-07-05 21:10:45 +0200

    Rewrite CategoryBar to TypeScript

--- Portal/src/components/CategoryBar.tsx
+++ Portal/src/components/CategoryBar.tsx
@@ -1,19 +1,26 @@
 import { Link } from "react-router-dom"
 import { TailSpin } from "react-loader-spinner"
+import type { Category } from "../types/CoreSwaggerTypes.ts"
+import AppLink from "./AppLink.tsx"
+import { getEntityPrettyName } from "../utils/formattingUtils.ts"
 
-const loadingCategoriesCount = 3
+const LOADING_CATEGORIES_COUNT = 3
 
-export default function CategoryBar({ categories }) {
+interface CategoryBarProps {
+    categories: Category[] | null
+}
+
+export default function CategoryBar({ categories }: CategoryBarProps) {
     return (!categories || categories.length > 0) && (
         <div className="flex flex-col lg:flex-row justify-center gap-3 px-4 my-4">
-            {categories ? categories.map(({ id, name }) => (
-                <Link
-                    key={id}
-                    to={`${window.location.pathname.startsWith("/plan") ? "/plan" : ""}/category/${id}`}
+            {categories ? categories.map(category => (
+                <AppLink
+                    key={category.id}
+                    to={category}
                     className="flex w-full lg:w-auto text-center items-center justify-center px-4 py-2 bg-white rounded-lg shadow text-sm font-medium hover:bg-gray-100 transition">
-                    {name}
-                </Link>
-            )) : Array.from({ length: loadingCategoriesCount }).map((_, index) => (
+                    {getEntityPrettyName(category.name)}
+                </AppLink>
+            )) : Array.from({ length: LOADING_CATEGORIES_COUNT }).map((_, index) => (
                 <div
                     key={index}
                     className="flex w-full lg:w-auto text-center items-center justify-center px-4 py-2 bg-white rounded-lg shadow text-sm font-medium hover:bg-gray-100 transition">
