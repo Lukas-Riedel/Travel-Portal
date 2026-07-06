eab3e08091ec5bb86264a4b798fbed551c6f93e2
Lukáš Riedel <thespeedding@gmail.com>
2026-07-06 21:46:03 +0200

    Rewrite Tooltip to TypeScript

--- Portal/src/components/Tooltip.tsx
+++ Portal/src/components/Tooltip.tsx
@@ -1,4 +1,8 @@
-export default function Tooltip({ children }) {
+interface TooltipProps {
+    children: React.ReactNode
+}
+
+export default function Tooltip({ children }: TooltipProps) {
     return (
         <div className="absolute bottom-full left-1/2 -translate-x-1/2 mb-2 hidden group-hover:flex
             items-center gap-1 bg-black text-white text-sm px-2 py-1 rounded whitespace-nowrap z-10">
