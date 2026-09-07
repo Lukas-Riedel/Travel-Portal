6268c4b3077d5a0947ee4ffc479360d685dc51d3
Lukáš Riedel <thespeedding@gmail.com>
2026-09-07 15:33:33 +0200

    Rewrite RecentPlacesPage to TypeScript

--- Portal/src/pages/RecentPlacesPage.tsx
+++ Portal/src/pages/RecentPlacesPage.tsx
@@ -1,39 +1,40 @@
-import { useCategories } from "../hooks/useCategories.js"
-import MapFrame from "../components/MapFrame.tsx"
 import { useEffect, useMemo, useRef, useState } from "react"
+import { useAuth } from "../contexts/AuthContext.jsx"
+import { useEvents } from "../hooks/useEvents.ts"
+import { useRegularPlaces } from "../hooks/useRegularPlaces.ts"
+import { useTimeFilteredRegularPlaces } from "../hooks/useTimeFilteredRegularPlaces.ts"
+import { useCategories } from "../hooks/useCategories.js"
+import { useUpcomingOrCurrentTrip } from "../hooks/useUpcomingOrCurrentTrip.js"
+import MapFrame from "../components/MapFrame.tsx"
 import PlaceMap from "../components/PlaceMap.jsx"
 import PlaceSummaryList from "../components/PlaceSummaryList.jsx"
-import { TailSpin } from "react-loader-spinner"
-import { useAuth } from "../contexts/AuthContext.jsx"
-import { useUpcomingOrCurrentTrip } from "../hooks/useUpcomingOrCurrentTrip.js"
 import TripSummary from "../components/TripSummary.tsx"
-import { UserRole } from "../types/CoreSwaggerTypes.ts"
-import { useRegularPlaces } from "../hooks/useRegularPlaces.ts"
+import { TailSpin } from "react-loader-spinner"
+import { CategoryCategory, PlaceIncludedEntity, PlaceSortingStrategy, UserRole } from "../types/CoreSwaggerTypes.ts"
 import { getCurrentOrMaximumAllowedTimestamp, getMaximumAllowedTimetamp } from "../utils/timeUtils.ts"
-import { useTimeFilteredRegularPlaces } from "../hooks/useTimeFilteredRegularPlaces.ts"
-import { useEvents } from "../hooks/useEvents.ts"
+import { useCountryCategoriesMap } from "../hooks/useCountryCategoriesMap.ts"
+import { Place } from "../classes/Place.ts"
 
-const limitStep = 10
-const maxDistance = 2000
+const LIMIT_STEP = 10
+const MAX_DISTANCE = 2000
 
 export default function RecentPlacesPage() {
     const { hasRole } = useAuth()
     const { publishPhotosUploadingTriggeredEvent } = useEvents()
 
-    const [displayedPlaces, setDisplayedPlaces] = useState(undefined)
-    const [currentLimit, setCurrentLimit] = useState(limitStep)
+    const [displayedPlaces, setDisplayedPlaces] = useState<Place[] | null>(null)
+    const [currentLimit, setCurrentLimit] = useState(LIMIT_STEP)
     const isFetching = useRef(false)
 
-    const { places } = useRegularPlaces({ include: ["categories", "dates", "excerpt"], limit: currentLimit, maxEnd: getCurrentOrMaximumAllowedTimestamp(), sort: "-oldestWithTrip" })
-    const { places: allPlaces } = useTimeFilteredRegularPlaces({ sort: "-score" })
-    const countryCategories = useCategories({ categories: ["country"] })
-
+    const { places } = useRegularPlaces({ include: [PlaceIncludedEntity.Categories, PlaceIncludedEntity.Dates, PlaceIncludedEntity.Excerpt], limit: currentLimit, maxEnd: getCurrentOrMaximumAllowedTimestamp(), sort: PlaceSortingStrategy.ValueOldestWithTrip })
+    const { places: allPlaces } = useTimeFilteredRegularPlaces({ sort: PlaceSortingStrategy.ValueScore })
+    const countryCategoriesMap = useCountryCategoriesMap()
     const { trip: upcomingOrCurrentTrip, createTripNote, removeTripNote } = useUpcomingOrCurrentTrip()
 
     useEffect(() => {
         if (places?.length) {
-            if (places.length === limitStep) {
-                const breakIndex = places.findIndex((place, i) => i === 0 ? false : place.getHaversineDistanceTo(places[i - 1]) > maxDistance)
+            if (places.length === LIMIT_STEP) {
+                const breakIndex = places.findIndex((place, i) => i === 0 ? false : place.getHaversineDistanceTo(places[i - 1]) > MAX_DISTANCE)
 
                 const filteredPlaces = breakIndex === -1 ? places : places.slice(0, breakIndex)
                 setDisplayedPlaces(filteredPlaces)
@@ -55,7 +56,7 @@ export default function RecentPlacesPage() {
             }
 
             isFetching.current = true
-            setCurrentLimit(prev => prev + limitStep)
+            setCurrentLimit(previous => previous + LIMIT_STEP)
             setTimeout(() => {
                 isFetching.current = false
             }, 300)
@@ -64,10 +65,6 @@ export default function RecentPlacesPage() {
         window.addEventListener("scroll", onScroll)
         return () => window.removeEventListener("scroll", onScroll)
     }, [])
-
-    const countryCategoriesMap = useMemo(() => {
-        return new Map(countryCategories?.map(category => [category.name, category]))
-    }, [countryCategories])
 
     return hasRole(UserRole.PlaceRead) && (
         <>
