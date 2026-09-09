b605642f60f74d9bed77cb83c2c3d7db9c6ecc9f
Lukáš Riedel <thespeedding@gmail.com>
2026-09-09 16:06:45 +0200

    Sort Gemini models by newest version

--- Core/src/Client/GenerativeContent/GeminiGenerativeContentClient.php
+++ Core/src/Client/GenerativeContent/GeminiGenerativeContentClient.php
@@ -13,8 +13,9 @@
         private const GENERATE_CONTENT_URL_FORMAT = "https://generativelanguage.googleapis.com/v1beta/%s:generateContent?key=%s";
 
         private const GENERATE_CONTENT_GENERATION_METHOD = "generateContent";
-        private const LATEST_MODEL_TAG = "latest";
-        private const GEMINI_MODEL_NAME = "gemini";
+        private const MODEL_NAME = "gemini";
+        private const MODEL_WORDS_WHITELIST = array("flash", "pro");
+        private const MODEL_WORDS_BLACKLIST = array("image", "latest", "preview", "omni");
 
         private const KEY_PLACEHOLDER_FORMAT = "{%s}";
 
@@ -71,8 +72,14 @@
             }
 
             $response = $this->httpClient->executeRequest(HttpMethod::GET, sprintf(self::GET_MODELS_URL_FORMAT, $this->googleGeminiApiKey))["models"];
-            $models = array_map(fn($model) => $model["name"], array_values(array_filter($response, fn($model) => isset($model["thinking"]) && $model["thinking"]
-                && !str_contains($model["name"], self::LATEST_MODEL_TAG) && str_contains($model["name"], self::GEMINI_MODEL_NAME) && in_array(self::GENERATE_CONTENT_GENERATION_METHOD, $model["supportedGenerationMethods"]))));
+            $models = array_map(fn($model) => $model["name"], array_values(array_filter($response, fn($model) => isset($model["thinking"]) && $model["thinking"] && str_contains($model["name"], self::MODEL_NAME)
+                && count(array_filter(self::MODEL_WORDS_WHITELIST, fn($allowed) => str_contains($model["name"], $allowed))) > 0
+                && count(array_filter(self::MODEL_WORDS_BLACKLIST, fn($forbidden) => str_contains($model["name"], $forbidden))) === 0
+                && in_array(self::GENERATE_CONTENT_GENERATION_METHOD, $model["supportedGenerationMethods"]))));
+            
+            usort($models, function($a, $b) {
+                return version_compare($b, $a);
+            });
 
             $this->distributedCacheClient->set(self::MODELS_CACHE_KEY, $models, self::MODELS_CACHE_TTL);
 
